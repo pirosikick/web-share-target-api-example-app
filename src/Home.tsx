20d6e45a6e34fcc1bca33f4db543aefcf3f8ea1f
Hiroyuki Anai <pirosikick@gmail.com>
2019-09-13 09:51:36 +0900

    Add unsupported message

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,12 +3,12 @@ import React, { useState, useEffect } from "react";
 const DEFAULT_TEXT = "hoge";
 
 const Home: React.FC = () => {
-  const [supportWebShare, setSupportWebShare] = useState(false);
+  const [webShareSupported, setWebShareSupported] = useState(false);
   const [title, setTitle] = useState("");
   const [url, setURL] = useState("");
   const [text, setText] = useState(DEFAULT_TEXT);
   useEffect(() => {
-    setSupportWebShare("share" in navigator);
+    setWebShareSupported("share" in navigator);
     setTitle(document.title);
     setURL(window.location.href);
   }, []);
@@ -26,9 +26,9 @@ const Home: React.FC = () => {
   return (
     <div style={{ padding: "10px" }}>
       <h1>Hello. This App is Web Share Target API Example</h1>
-      {supportWebShare && (
+      <h2>Web Share API</h2>
+      {webShareSupported ? (
         <>
-          <h2>Web Share API</h2>
           <h3>Title</h3>
           <p>{title}</p>
           <h3>URL</h3>
@@ -40,6 +40,8 @@ const Home: React.FC = () => {
           ></textarea>
           <button onClick={handleClick}>Share</button>
         </>
+      ) : (
+        <p>In this browser, Web Share API is not supported.</p>
       )}
     </div>
   );
